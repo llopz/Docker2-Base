2a5ac07bd88748c61c4b39ce525c15649b9f43e4
Spooky-Donut <jairoluismoreno2@gmail.com>
2025-02-09 17:55:33 -0500

    Se organizan en formato tabla los resultados

--- Base.sh
+++ Base.sh
@@ -6,26 +6,35 @@ sleep 10
 
 git clone https://github.com/llopz/PC2-Docker2.git
 
+echo "" >> $TIMES
+
 cd PC2-Docker2/C++
 docker build -t c_image -f cdockerfile .
-docker run --privileged --rm c_image
+c_time=$(docker run --privileged --rm c_image)
 cd ../..
 
 cd PC2-Docker2/Java
 docker build -t j_image -f jdockerfile .
-docker run --privileged --rm j_image
+j_time=$(docker run --privileged --rm j_image)
 cd ../..
 
 cd PC2-Docker2/JavaScript
 docker build -t js_image -f jsdockerfile .
-docker run --privileged --rm js_image
+js_time=$(docker run --privileged --rm js_image)
 cd ../..
 
 cd PC2-Docker2/Go
 docker build -t g_image -f gdockerfile .
-docker run --privileged --rm g_image
+g_time=$(docker run --privileged --rm g_image)
 cd ../..
 
 cd PC2-Docker2/Python
 docker build -t p_image -f pydockerfile .
-docker run --privileged --rm p_image
+p_time=$(docker run --privileged --rm p_image)
+
+echo -e "-----RESULTADOS-----"
+echo $c_time
+echo $j_time
+echo $js_time
+echo $g_time
+echo $p_time
